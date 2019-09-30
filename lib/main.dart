ade9212febe29d1f25aefb759c34c5e160411027
vmihalachi <vmihalachi@outlook.com>
2019-09-30 23:10:20 +0300

    stuff

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.black,
             body: ResponsiveLayout(
-              largeScreen:HomePageLarge(),
+              largeScreen: HomePageLarge(),
               smallScreen: HomePage(),
             )));
   }
@@ -70,7 +70,9 @@ class _Headline extends StatelessWidget {
       Text(
         "Full-Stack developer, App developer, Server Guy. Special care for design architectures & code reusability. "
         "Freelancer and co-founder of Globl Agency and Docode OÜ.",
-        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w200),
+        style: TextStyle(color: Colors.grey),
+        textScaleFactor: 1.3,
+        // textAlign: TextAlign.justify,
       ),
       SizedBox(height: _kSpace * 2),
       RaisedButton(
@@ -86,31 +88,42 @@ class _Headline extends StatelessWidget {
 }
 
 class _Projects extends StatelessWidget {
-  static const _kProjects = [
+
+  static final _kGloblAgencyColor = Colors.green[700];
+  static const _kDocodeColor = Color(0xFF0092FF);
+
+  static final _kProjects = [
     Project(
-        title: 'Beatpulse - Globl Agency',
-        description: 'Producer-owned beat streaming and licensing platform.',
-        url: 'https://beatpulse.co'),
+      title: 'Beatpulse - Globl Agency',
+      description: 'Producer-owned beat streaming and licensing platform.',
+      url: 'https://beatpulse.co',
+      color: _kGloblAgencyColor,
+    ),
     Project(
         title: 'Nyam - Docode OÜ',
         description: 'Complete platform for restaurants that do not want to compromise.',
-        url: 'https://docode.it/nyam'),
+        url: 'https://docode.it/nyam',
+      color: _kDocodeColor),
     Project(
         title: 'Task Runner - Globl Agency',
         description: "You API's best friend. Run your periodic and scheduled tasks with ease.",
-        url: 'https://taskrunner.run'),
+        url: 'https://taskrunner.run',
+      color: _kGloblAgencyColor),
     Project(
         title: 'Turbo Client',
         description: "Sleek, intuitive and powerful Android FTP & SFTP client.",
-        url: 'https://play.google.com/store/apps/details?id=turbo.client'),
+        url: 'https://play.google.com/store/apps/details?id=turbo.client',
+        color: _kPrimaryColor),
     Project(
         title: 'Turbo Editor',
         description: "Simple, powerful and Open Source text editor for Android.",
-        url: 'https://play.google.com/store/apps/details?id=com.maskyn.fileeditor'),
+        url: 'https://play.google.com/store/apps/details?id=com.maskyn.fileeditor',
+        color: _kPrimaryColor),
     Project(
         title: 'Just Quote It',
         description: "Get inspired everyday with this delightful mobile app.",
-        url: 'https://play.google.com/store/apps/details?id=com.vmihalachi.justquoteit'),
+        url: 'https://play.google.com/store/apps/details?id=com.vmihalachi.justquoteit',
+        color: _kPrimaryColor),
   ];
 
   const _Projects({Key key}) : super(key: key);
@@ -120,8 +133,9 @@ class _Projects extends StatelessWidget {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       for (final project in _kProjects) ...[
         FlatButton(
-          padding: EdgeInsets.all(16),
-          color: Colors.grey[800],
+          padding: EdgeInsets.all(24),
+          color: Colors.grey[900],
+          hoverColor: project.color,
           onPressed: () {
             html.window.open(project.url, '_blank');
           },
@@ -133,9 +147,10 @@ class _Projects extends StatelessWidget {
                   style: TextStyle(fontWeight: FontWeight.bold),
                   textScaleFactor: 1.1,
                 ),
+                SizedBox(height: 4),
                 Text(
                   project.description,
-                  style: TextStyle(fontWeight: FontWeight.w200),
+                  style: TextStyle(color: Colors.grey),
                 ),
               ])),
         ),
--- lib/project.dart
+++ lib/project.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 
 class Project {
   final String title;
   final String description;
   final String url;
+  final Color color;
 
-  const Project({@required this.title, @required this.description, @required this.url});
+  const Project({@required this.title, @required this.description, @required this.url, @required this.color});
 }
